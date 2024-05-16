fa3e8c1daa55eb4b2bc48ccae2538c00be72963b
nexo <65514701+whzinn@users.noreply.github.com>
2024-05-15 23:33:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/clique')
 def clique():
     d = incrementa_acesso()
-    return f"Pagina Online"
+    return d
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
