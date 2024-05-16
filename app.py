7f58f14f1dc0a6bbc634e1bfc50ba6b247d249f6
nexo <65514701+whzinn@users.noreply.github.com>
2024-05-15 23:16:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os
 import time
+from db import incrementa_acesso
 
 app = Flask(__name__)
 
 @app.route('/clique')
 def clique():
+    d = incrementa_acesso()
     return f"Pagina Online"
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
